c585dab9c7da03768c0c632fc55375c63b8c4a40
HenrikJava <henriklarssonmail@gmail.com>
2020-11-19 18:39:05 +0100

    feat: cityinput components

---
+++ client/src/components/cityInput/CityInput.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {CityContext} from '../../shared/global/provider/CityProvider'
+import {DataContext} from '../../shared/global/provider/DataProvider'
+import './CityInput.css'
+import { useContext, useEffect } from 'react'
+import WeatherService from '../../shared/api/service/WeatherService'
+
+export  const CityInput = () => {
+    const [city, setCity] = useContext(CityContext)
+    const [data, setData] = useContext(DataContext)
+    useEffect(() =>{ fetchDataFromExternalApi()}, []);
+
+  const fetchDataFromExternalApi = (event) => {
+    
+    WeatherService.searchCity(city)
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  };
+    return(
+<div className="cityInputWrapper"><form onSubmit={(e) => {e.preventDefault(); fetchDataFromExternalApi()}}><input className="input" type="text" onChange={(event) => setCity(event.target.value)} placeholder="Sök stad här"/>
+      <input type="submit" value="Sök"></input></form>
+      </div>
+    )
+}
