1adb5613537b6ec9e2c7efc76f2b71b13ed7968b
HenrikJava <henriklarssonmail@gmail.com>
2020-11-20 19:01:03 +0100

    fix: cleaning up app and home comp while moving it inside

--- client/src/view/HomeView.js
+++ client/src/view/HomeView.js
@@ -1,31 +1,12 @@
-import React, {  useContext } from "react";
-import {DataContext} from '../shared/global/provider/DataProvider'
-import {CityInput} from '../components/cityInput/CityInput'
+import React from "react";
+import { CityInput } from "../components/cityInput/CityInput";
+import { WeatherCard } from "../components/weathercard/WeatherCard";
+
 export const HomeView = () => {
-  const [data, setData] = useContext(DataContext)
-
-  
-  const displayData = () => {
-    if (data) {
-      return (
-        <div>
-          <h1>{data.city.name}</h1>
-          Today
-          <h2>{data.list[0].weather[0].main}</h2>
-      <h3>{data.list[0].main.temp}</h3>
-      Forecast
-          <h2>{data.list[39].weather[0].main}</h2>
-      <h3>{data.list[39].main.temp}</h3>
-        </div>
-      );
-    }
-  };
-
   return (
     <div>
-      
       <CityInput></CityInput>
-  <span>{displayData()}</span>
+      <WeatherCard></WeatherCard>
     </div>
   );
 };
