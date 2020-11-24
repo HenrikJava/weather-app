4e2582086c267e5f07e4561a15fd254b3bccd68e
HenrikJava <henriklarssonmail@gmail.com>
2020-11-24 14:22:42 +0100

    style: some global styling and renaming

---
+++ client/src/components/weathercards/WeatherCards.js
@@ -0,0 +1,33 @@
+import React, { useContext, useEffect } from "react";
+import { DataContext } from "../../shared/global/provider/DataProvider";
+import { CityContext } from "../../shared/global/provider/CityProvider";
+import { Card } from "@material-ui/core";
+import { CurrentWeather } from "../currentWeather/CurrentWeather";
+import { ForeCastWeather } from "../forecastWeather/ForeCastWeather";
+import WeatherService from "../../shared/api/service/WeatherService";
+import "./WeatherCards.css";
+export const WeatherCards = () => {
+  const [data, setData] = useContext(DataContext);
+  const [city, setCity] = useContext(CityContext);
+  useEffect(() => {
+    fetchDataFromExternalApi();
+  }, []);
+
+  const fetchDataFromExternalApi = () => {
+    WeatherService.searchCity(city)
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  };
+  const displayData = () => {
+    if (data) {
+      return (
+        <div className="weather-cards">
+          <CurrentWeather></CurrentWeather>
+          <ForeCastWeather></ForeCastWeather>
+        </div>
+      );
+    }
+    else return null
+  };
+  return ( displayData() )
+};
