0d22bc6d066a828a220cb20c8350a786ce2c5f2d
HenrikJava <henriklarssonmail@gmail.com>
2020-11-24 14:23:20 +0100

    feat: cityinput and currentweather

--- client/src/components/cityInput/CityInput.js
+++ client/src/components/cityInput/CityInput.js
@@ -1,24 +1,35 @@
-import React from 'react'
-import {CityContext} from '../../shared/global/provider/CityProvider'
-import {DataContext} from '../../shared/global/provider/DataProvider'
-import './CityInput.css'
-import { useContext, useEffect } from 'react'
-import WeatherService from '../../shared/api/service/WeatherService'
+import React from "react";
+import { CityContext } from "../../shared/global/provider/CityProvider";
+import { DataContext } from "../../shared/global/provider/DataProvider";
+import "./CityInput.css";
+import { useContext } from "react";
+import WeatherService from "../../shared/api/service/WeatherService";
 
-export  const CityInput = () => {
-    const [city, setCity] = useContext(CityContext)
-    const [data, setData] = useContext(DataContext)
-    useEffect(() =>{ fetchDataFromExternalApi()}, []);
-
-  const fetchDataFromExternalApi = (event) => {
-    
+export const CityInput = () => {
+  const [city, setCity] = useContext(CityContext);
+  const [data, setData] = useContext(DataContext);
+  const fetchDataFromExternalApi = () => {
     WeatherService.searchCity(city)
       .then((response) => setData(response.data))
       .catch((error) => console.log(error));
   };
-    return(
-<div className="cityInputWrapper"><form onSubmit={(e) => {e.preventDefault(); fetchDataFromExternalApi()}}><input className="input" type="text" onChange={(event) => setCity(event.target.value)} placeholder="Search city..."/>
-      <input type="submit" value="Search"></input></form>
-      </div>
-    )
-}
+  return (
+    <div className="city-input-wrapper">
+      <form id="cityForm"
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetchDataFromExternalApi();
+          e.target.reset()
+        }}
+      >
+        <input
+          className="input"
+          type="text"
+          onChange={(event) => setCity(event.target.value)}
+          placeholder="Search city..."
+        />
+        <input type="submit" value="Search"></input>
+      </form>
+    </div>
+  );
+};
--- client/src/components/currentWeather/CurrentWeather.js
+++ client/src/components/currentWeather/CurrentWeather.js
@@ -1,24 +1,28 @@
 import React, { useContext } from "react";
 import { DataContext } from "../../shared/global/provider/DataProvider";
 import { Typography } from "@material-ui/core";
+import { CityInfo } from "../../components/cityInfo/CityInfo";
+import { CityInput } from "../../components/cityInput/CityInput";
 import './CurrentWeather.css'
 
 export const CurrentWeather = () => {
   const [data] = useContext(DataContext);
-
+console.log(data);
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
   return (
-    <div>
+    <div className="current-weather-wrapper">
+      <CityInput></CityInput>
+      <CityInfo></CityInfo>
       <Typography variant="h3">
         {capitalizeFirstLetter(data.list[0].weather[0].description)}
       </Typography>
       <div className="image-and-temp">
-      <img
+      <img className="weather-icon"
         src={`http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`} alt="Weather Icon"
       ></img>
-      <Typography variant="h3">{Math.round(data.list[0].main.temp)+`°`}</Typography>
+      <Typography variant="h1">{Math.round(data.list[0].main.temp)+`°`}</Typography>
       </div>
     </div>
   );
