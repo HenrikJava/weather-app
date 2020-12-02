90376022162f9e91ce483f74689a0129ee967cb0
HenrikJava <henriklarssonmail@gmail.com>
2020-12-02 15:00:29 +0100

    feat: made the clock ticking

--- client/src/components/cityInfo/CityInfo.js
+++ client/src/components/cityInfo/CityInfo.js
@@ -1,21 +1,24 @@
-import React, {useContext} from 'react'
-import {WeatherContext} from '../../shared/global/provider/WeatherProvider'
-import {Typography} from '@material-ui/core'
-import {calcTime} from '../../shared/global/functions'
+import React, { useContext, useEffect, useState } from "react";
+import { WeatherContext } from "../../shared/global/provider/AppProvider";
+import { calcTime } from "../../shared/global/functions";
+import './CityInfo.css'
 export const CityInfo = () => {
-    const [weather] = useContext(WeatherContext)
-
-    
-    return(
-        <div><Typography variant="h3">
-        {`${weather.city.name}, ${weather.city.country}`}
-      </Typography><Typography variant="h6">
-        {`The local time in  ${weather.city.name} is ${calcTime(weather.city.timezone).toLocaleString()}`}
-      </Typography></div>
-
-    )
-}
-
-
-
-
+  const [weather] = useContext(WeatherContext);
+    const [time, setTime] = useState("N/A")
+useEffect(() => {
+  const interval = setInterval(() => {
+         setTime(calcTime(
+      weather.city.timezone
+    ).toLocaleString())
+  }, 1000);
+  return () => clearInterval(interval);
+}, [weather]);
+  return (
+    <div>
+      <h1 id="city-info">{`${weather.city.name}, ${weather.city.country}`}</h1>
+      <h3 id="time">
+        {`The local time in  ${weather.city.name} is ${time}`}
+      </h3>
+    </div>
+  );
+};
