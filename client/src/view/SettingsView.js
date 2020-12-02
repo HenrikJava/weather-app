96a865508f026839003f17e11f3c0f76a8c79e31
HenrikJava <henriklarssonmail@gmail.com>
2020-12-02 15:49:08 +0100

    feat: created settings view

--- client/src/view/SettingsView.js
+++ client/src/view/SettingsView.js
@@ -1,7 +1,38 @@
-import React from 'react'
-
+import React, { useContext, useState } from "react";
+import Switch from "@material-ui/core/Switch";
+import Grid from "@material-ui/core/Grid";
+import { UserContext} from '../shared/global/provider/AppProvider'
+import "./SettingsView.css";
 export const SettingsView = () => {
-    return(
-        <div>Settings</div>
-    )
-}
+  const [authenticatedUser, setAuthenticatedUser,firstname,
+    setFirstname,
+    lastname,
+setLastname,username, setUsername,password, setPassword, mail, setMail,favoriteCity, setFavoriteCity,celciusOn, setcelciusOn] = useContext(UserContext);
+  const handleChange = () => {
+    setcelciusOn(!celciusOn);
+  };
+
+  return (
+    <div className="settings-view">
+      <Grid container id="settings-box">
+        <Grid item xs={12} id="settings-header">
+          Settings
+        </Grid>
+
+        <Grid item xs={6} id="temp-name">
+          Temperature:
+        </Grid>
+        <Grid item xs={6} id="switch">
+          <p>Fahrenheit</p>
+          <Switch
+            checked={celciusOn}
+            onChange={handleChange}
+            color="primary"
+            inputProps={{ "aria-label": "primary checkbox" }}
+          />
+          <p>Celcius</p>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
