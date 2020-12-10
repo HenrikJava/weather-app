cac26bdcd51c5487fb2bc7387c710e1b7dde7eb5
HenrikJava <henriklarssonmail@gmail.com>
2020-12-10 08:33:58 +0100

    feat: created the service for the user

---
+++ client/src/shared/api/service/UserService.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+
+import {setAuthToken} from '../../global/functions'
+
+export const loadUser = async () => {
+  
+  if (localStorage.token) {
+setAuthToken(localStorage.token)
+  }
+  try {
+    const res = await axios.get('/api/auth')
+    return res
+
+  } catch(err) {
+    localStorage.removeItem("token");
+      return null
+
+
+  } 
+}
+
+
+export const registerUser = async (values) => {
+  
+  
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const body = JSON.stringify(values);
+  try {
+    const res = await axios.post("/api/user", body, config);
+    if (res.status === 200) {
+      
+      localStorage.setItem("token", res.data.token);
+      return res
+    } else {
+      localStorage.removeItem("token");
+      return null
+
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  return null
+};
+
+export const loginUser = async (email, password) => {
+  
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const body = JSON.stringify({email, password});
+  try {
+    const res = await axios.post("/api/auth", body, config);
+    if (res.status === 200) {
+      
+      localStorage.setItem("token", res.data.token);
+      setAuthToken(localStorage.token);
+      return res
+    } else {
+      localStorage.removeItem("token");
+      return null
+
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  return null
+};
