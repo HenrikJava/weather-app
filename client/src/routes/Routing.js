ca828935652352c1a1441483105957e611b43295
HenrikJava <henriklarssonmail@gmail.com>
2020-12-11 09:53:32 +0100

    moved fetch weater to router if refreshing the page on profile or settingspage

--- client/src/routes/Routing.js
+++ client/src/routes/Routing.js
@@ -1,53 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { HomeView } from "../view/HomeView";
 import { ProfileView } from "../view/ProfileView";
 import { EmptyView } from "../view/EmptyView";
 import { SettingsView } from "../view/SettingsView";
 import { useEffect, useContext } from "react";
-import { UserContext } from "../shared/global/provider/Provider";
-import { loadUser } from "../shared/api/service/UserService";
+import {
+  UserContext,
+  AppContext,
+  WeatherContext,
+} from "../shared/global/provider/Provider";
 import RoutingPath from "./RoutingPath";
+import WeatherService from "../shared/api/service/WeatherService";
+import { loadUser } from "../shared/api/service/UserService";
+import { Error } from "../components/error/Error";
 
 export const Routing = (props) => {
   const user = useContext(UserContext);
+  const [weather, setWeather] = useContext(WeatherContext);
+  const app = useContext(AppContext);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const loggedInUser = await loadUser();
+      if (loggedInUser) {
+        user.setFirstname(loggedInUser.data.firstname);
+        user.setLastname(loggedInUser.data.lastname);
+        user.setEmail(loggedInUser.data.email);
+        user.setAvatar(loggedInUser.data.avatar);
+        user.setAuthenticatedUser(true);
+        user.setFavouriteCity(
+          loggedInUser.data.favourite_city
+            ? loggedInUser.data.favourite_city
+            : ""
+        );
+        app.setFahrenheitOn(
+          loggedInUser.data.fahrenheit_on
+            ? loggedInUser.data.fahrenheit_on
+            : false
+        );
+        WeatherService.searchCity(
+          loggedInUser.data.favourite_city
+            ? loggedInUser.data.favourite_city
+            : app.city,
+          loggedInUser.data.fahrenheit_on ? true : false
+        )
+          .then((response) => setWeather(response.data))
+          .catch((error) => {
+            console.log(error);
+            setError(true);
+          });
+      } else {
+        WeatherService.searchCity(app.city, app.fahrenheitOn)
+          .then((response) => {
+            setWeather(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+            setError(true);
+          });
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const blockRouteIfNotAuthenticated = (navigateToView) => {
     if (!user.authenticatedUser) {
       return EmptyView;
     } else return navigateToView;
   };
-  useEffect(() => {
-   
-    const fetchData = async () => {
-      const loggedInUser = await loadUser();
 
-      if (loggedInUser) {
-        user.setFirstname(loggedInUser.data.firstname);
-        user.setLastname(loggedInUser.data.lastname);
-        user.setEmail(loggedInUser.data.email);
-        user.setFavouriteCity(loggedInUser.data.favourite_city);
-        user.setAvatar(loggedInUser.data.avatar);
-        user.setAuthenticatedUser(true);
-        user.setCelciusOn(loggedInUser.data.celciusOn)
-      }
-    };
-    
-    fetchData();
-  }, []);
-  return (
-    <Router>
-      {props.children}
-      <Switch>
-        <Route exact path={RoutingPath.homeView} component={HomeView}></Route>
-        <Route
-          path={RoutingPath.profileView}
-          component={blockRouteIfNotAuthenticated(ProfileView)}
-        ></Route>
-        <Route path={RoutingPath.settingsView} component={SettingsView}></Route>
+  const displayRouter = () => {
+    if (weather) {
+      return (
+        <Router>
+          {props.children}
+          <Switch>
+            <Route
+              exact
+              path={RoutingPath.homeView}
+              component={HomeView}
+            ></Route>
+            <Route
+              path={RoutingPath.profileView}
+              component={blockRouteIfNotAuthenticated(ProfileView)}
+            ></Route>
+            <Route
+              path={RoutingPath.settingsView}
+              component={SettingsView}
+            ></Route>
 
-        <Route component={HomeView}></Route>
-      </Switch>
-    </Router>
-  );
+            <Route component={HomeView}></Route>
+          </Switch>
+        </Router>
+      );
+    } else if (error) {
+      return <Error></Error>;
+    } else return null;
+  };
+
+  return displayRouter();
 };
--- client/src/view/HomeView.js
+++ client/src/view/HomeView.js
@@ -5,32 +5,22 @@ import { MainWeather } from "../components/mainWeather/MainWeather";
 import { ForeCastWeather } from "../components/forecastWeather/ForeCastWeather";
 import WeatherService from "../shared/api/service/WeatherService";
 import { UserContext } from "../shared/global/provider/Provider";
-import {Error} from "../components/error/Error"
+import { Error } from "../components/error/Error";
+import { loadUser } from "../shared/api/service/UserService";
+
 import "./HomeView.css";
 export const HomeView = () => {
-  const user = useContext(UserContext);
-  const [weather, setWeather] = useContext(WeatherContext);
-  const app = useContext(AppContext);
-  const [error, setError] = useState()
 
-  useEffect(() => {
-    WeatherService.searchCity(app.city, user.celciusOn)
-      .then((response) => setWeather(response.data))
-      .catch((error) => {console.log(error); setError(true)});
-  }, []);
 
   const displayWeather = () => {
-    if (weather) {
+    
       return (
         <div className="home-container">
           <MainWeather></MainWeather>
           <ForeCastWeather></ForeCastWeather>
         </div>
       );
-    } 
-    if (error) return <Error></Error>;
-    else return null
-  
+    
   };
   return displayWeather();
 };
