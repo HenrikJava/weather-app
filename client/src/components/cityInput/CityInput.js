31a7e408e6e8d5860204d25079158cc843fb3260
HenrikJava <henriklarssonmail@gmail.com>
2020-12-17 16:14:55 +0100

    feat: functionality to search for default city if search is invalid

--- client/src/components/cityInput/CityInput.js
+++ client/src/components/cityInput/CityInput.js
@@ -2,27 +2,37 @@ import React from "react";
 import { AppContext } from "../../shared/global/provider/Provider";
 import { WeatherContext } from "../../shared/global/provider/Provider";
 import "./CityInput.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import WeatherService from "../../shared/api/service/WeatherService";
 import SearchIcon from "@material-ui/icons/Search";
 import Input from "@material-ui/core/Input";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import Button from "@material-ui/core/Button";
-import { UserContext } from "../../shared/global/provider/Provider";
 
 export const CityInput = () => {
-  const user = useContext(UserContext);
   const app = useContext(AppContext);
+  const [city, setCity] = useState();
   const [weather, setWeather] = useContext(WeatherContext);
-  const fetchDataFromExternalApi = () => {
-    WeatherService.searchCity(app.city, app.fahrenheitOn)
-      .then((response) => setWeather(response.data))
-      .catch((error) => console.log(error));
+  const fetchDataFromExternalApi = async () => {
+    const response = await WeatherService.searchCity(city, app.fahrenheitOn);
+    if (response.status === 200) {
+      app.setCity(response.data.city);
+      app.setnoCityText('');
+
+      setWeather(response.data);
+    } else if (
+      response.data.message.msgBody ===
+      "No city with that name in the database."
+    ) {
+      app.setnoCityText("No city with that name in the database.");
+    } else {
+      app.setnoCityText(response.data.message.msgBody);
+    }
   };
   return (
-    <div className="city-input-wrapper">
+    <div id="city-input-wrapper">
       <form
-        id="city-form"
+        id="form"
         onSubmit={(e) => {
           e.preventDefault();
           fetchDataFromExternalApi();
@@ -30,14 +40,13 @@ export const CityInput = () => {
         }}
       >
         <Input
-          className="city-input"
           type="text"
-          onChange={(event) => app.setCity(event.target.value)}
-          placeholder="Search city..."
+          onChange={(event) => setCity(event.target.value)}
+          placeholder="Enter city..."
           id="input-with-icon-adornment"
           endAdornment={
             <InputAdornment position="end">
-              <SearchIcon className="search-icon" />
+              <SearchIcon id="search-icon" />
             </InputAdornment>
           }
         />
--- client/src/shared/api/service/WeatherService.js
+++ client/src/shared/api/service/WeatherService.js
@@ -2,39 +2,42 @@ import WeatherService from "../WeatherApi";
 import axios from "axios";
 const weatherInstance = axios.create();
 
-const searchCity = async (city, fahrenheitOn) => {
+const searchCity = (city, fahrenheitOn) => {
   let scale;
   fahrenheitOn
     ? (scale = WeatherService.fahrenheit)
     : (scale = WeatherService.celcius);
 
-  try {
-    const response = await weatherInstance
-      .get(WeatherService.weatherApi + city + scale + WeatherService.apiKey);
-    if (response.status === 200) {
-      return response;
-    }
-  } catch (error) {
-    if (error.response.data.message === "city not found") {
-      return {
-        data: {
-          message: {
-            msgBody: "No city with that name, check favourite city and change it.",
-            msgError: true,
+  return weatherInstance
+    .get(WeatherService.weatherApi + city + scale + WeatherService.apiKey)
+    .then((response) => {
+      if (response.status === 200) {
+        return response;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      if (error.response.data.message === "city not found") {
+        return {
+          data: {
+            message: {
+              msgBody:
+                "No city with that name in the database.",
+              msgError: true,
+            },
           },
-        },
-      };
-    } else {
-      return {
-        data: {
-          message: {
-            msgBody: "Something wrong at server, please try again later.",
-            msgError: true,
+        };
+      } else {
+        return {
+          data: {
+            message: {
+              msgBody: "Something wrong at server, please try again later.",
+              msgError: true,
+            },
           },
-        },
-      };
-    }
-  }
+        };
+      }
+    });
 };
 
 export default { searchCity };
