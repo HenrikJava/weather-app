2d865fb16c86d9f0b0676e63715689833f3e627b
HenrikJava <henriklarssonmail@gmail.com>
2020-12-18 15:46:49 +0100

    feat: change the theme of the app to sloth

--- client/src/components/navigationBar/NavigationBar.js
+++ client/src/components/navigationBar/NavigationBar.js
@@ -4,6 +4,7 @@ import { useHistory } from "react-router-dom";
 import { Links } from "../links/Links";
 import RoutingPath from "../../routes/RoutingPath";
 import { AppContext } from "../../shared/global/provider/Provider";
+import defaultSloth from '../../shared/images/DefaultSloth.png'
 
 export const NavigationBar = () => {
   const history = useHistory();
@@ -11,6 +12,7 @@ export const NavigationBar = () => {
 
   return (
     <div className="navigation-bar-wrapper">
+     <img id="sloth" src={defaultSloth}></img>
       <h1
         className="app-name"
         onClick={() => {
@@ -20,7 +22,7 @@ export const NavigationBar = () => {
 
         }}
       >
-        Zebra weather
+        Sloth weather
       </h1>
       <span className="nav-links">
         <Links />
