6de4d3535bc6ff7086e4514419cc9d202f736fae
HenrikJava <henriklarssonmail@gmail.com>
2021-01-03 14:03:35 +0100

    feat: precipition function added

--- client/src/components/differentTimes/DifferentTimes.js
+++ client/src/components/differentTimes/DifferentTimes.js
@@ -10,8 +10,33 @@ import Grid from "@material-ui/core/Grid";
 export const DifferentTimes = () => {
   const [weather] = useContext(WeatherContext);
   const app = useContext(AppContext);
-  const timesArray = ["00:00:00","00:30:00","01:00:00","01:30:00","02:00:00","02:30:00", "06:00:00","06:30:00","07:00:00","07:30:00","08:00:00", "08:30:00","12:00:00" ,"12:30:00","13:00:00","13:30:00","14:00:00","14:30:00", "18:00:00","18:30:00","19:00:00","19:30:00","20:00:00","20:30:00"];
-  const weekday = []
+  const timesArray = [
+    "00:00:00",
+    "00:30:00",
+    "01:00:00",
+    "01:30:00",
+    "02:00:00",
+    "02:30:00",
+    "06:00:00",
+    "06:30:00",
+    "07:00:00",
+    "07:30:00",
+    "08:00:00",
+    "08:30:00",
+    "12:00:00",
+    "12:30:00",
+    "13:00:00",
+    "13:30:00",
+    "14:00:00",
+    "14:30:00",
+    "18:00:00",
+    "18:30:00",
+    "19:00:00",
+    "19:30:00",
+    "20:00:00",
+    "20:30:00",
+  ];
+  const weekday = [];
   weekday[0] = "Sunday";
   weekday[1] = "Monday";
   weekday[2] = "Tuesday";
@@ -19,11 +44,11 @@ export const DifferentTimes = () => {
   weekday[4] = "Thursday";
   weekday[5] = "Friday";
   weekday[6] = "Saturday";
-  
-  const getDayName = (fragment) => {
 
-    return weekday[new Date((fragment.dt+weather.city.timezone) * 1000).getUTCDay()]
-   
+  const getDayName = (fragment) => {
+    return weekday[
+      new Date((fragment.dt + weather.city.timezone) * 1000).getUTCDay()
+    ];
   };
   const getSpecificTimes = (time) => {
     let condition = false;
@@ -34,19 +59,61 @@ export const DifferentTimes = () => {
     });
     return condition;
   };
-  
-  
+
   const weatherAtCurrentDay = weather.list.filter(
     (fragment) => getDayName(fragment) === app.weekday
   );
-  const isToday =(new Date((weatherAtCurrentDay[0].dt+weather.city.timezone )* 1000).getUTCDay()===new Date(Date.now()+weather.city.timezone* 1000).getUTCDay())
-  let specificTimes = weatherAtCurrentDay.filter(
-    (fragment) => getSpecificTimes(fragment.dt_txt) === true
-  );
-  if (specificTimes.length===0) {
-    specificTimes.push(weatherAtCurrentDay[0])
+  const isToday =
+    new Date(
+      (weatherAtCurrentDay[0].dt + weather.city.timezone) * 1000
+    ).getUTCDay() ===
+    new Date(Date.now() + weather.city.timezone * 1000).getUTCDay();
+
+  let specificTimes = [];
+  let stampAdded = false;
+  weatherAtCurrentDay.forEach((timestamp) => {
+    if (stampAdded && timestamp.snow) {
+      
+      specificTimes[specificTimes.length - 1].precipitation += Number(
+        timestamp.snow["3h"]
+      );
+
+    } 
+    if (stampAdded && timestamp.rain) {
+      
+      specificTimes[specificTimes.length - 1].precipitation += Number(
+        timestamp.rain["3h"]
+      );
+
+    } 
+      if (getSpecificTimes(timestamp.dt_txt) === true) {
+        if (timestamp.snow) {
+          timestamp.precipitation = Number(timestamp.snow["3h"]);
+
+        } else if (timestamp.rain) {
+
+          timestamp.precipitation = Number(timestamp.rain["3h"]);
+        } else {
+
+          timestamp.precipitation = 0;
+          
+        }
+        specificTimes.push(timestamp);
+        stampAdded = true;
+
+      } else {
+        stampAdded = false;
+      }
+
+   
+  });
+
+  if (specificTimes.length === 0) {
+    specificTimes.push(weatherAtCurrentDay[0]);
   }
-  
+
+ 
+
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
@@ -65,18 +132,21 @@ export const DifferentTimes = () => {
           </Grid>
           <Grid item xs={1}>
             <p>
-              {Math.round(specificTimes[index].main.temp) + scale(app.fahrenheitOn)}
+              {Math.round(specificTimes[index].main.temp) +
+                scale(app.fahrenheitOn)}
             </p>
           </Grid>
           <Grid item xs={2}>
             <img
               className="details-weather-icon"
-              
               src={`/icons/${specificTimes[index].weather[0].icon}.png`}
               alt="Weather Icon"
             ></img>
           </Grid>
-          <Grid item xs={5}>
+          <Grid item xs={1}>
+            <p>{Math.ceil(specificTimes[index].precipitation) + " mm"}</p>
+          </Grid>
+          <Grid item xs={4}>
             <p>
               {capitalizeFirstLetter(
                 specificTimes[index].weather[0].description
@@ -99,7 +169,7 @@ export const DifferentTimes = () => {
 
   return (
     <div className="day-details">
-      <p className="day-to-display">{isToday? 'Today' :app.weekday}</p>
+      <p className="day-to-display">{isToday ? "Today" : app.weekday}</p>
 
       <div className="weather-at-differents-time-grid">
         {generateDifferentTimes()}
--- client/src/components/forecastWeather/DailyWeatherOverview.js
+++ client/src/components/forecastWeather/DailyWeatherOverview.js
@@ -11,6 +11,7 @@ export const DailyWeatherOverview = (props) => {
   const weather = useContext(
     WeatherContext
   );
+  let dailyPrecipitation = 0
   const weekday = []
 weekday[0] = "Sunday";
 weekday[1] = "Monday";
@@ -19,22 +20,31 @@ weekday[3] = "Wednesday";
 weekday[4] = "Thursday";
 weekday[5] = "Friday";
 weekday[6] = "Saturday";
-  const getDay = (dayLength) => {
-    if (dayLength === "long") {
-      return weekday[new Date((props.day.dt+weather[0].city.timezone )* 1000).getUTCDay()]
-      
-    } else {
+  const getDay = (timestamp) => {
+    if (timestamp) {
+      return new Date((timestamp+weather[0].city.timezone )* 1000).toUTCString().slice(0,3)
+
+    }
       return new Date((props.day.dt+weather[0].city.timezone )* 1000).toUTCString().slice(0,3)
       
-    }
+   
   };
+weather[0].list.forEach(element => {
+  
+  if (element.rain && getDay(element.dt)===getDay()) {
+    dailyPrecipitation += (Number(element.rain['3h']))
+  }
+  if (element.snow && getDay(element.dt)===getDay()) {
+    dailyPrecipitation += (Number(element.snow['3h']))
+  }
+});
   return (
     <div
       className={` daily-weather template${props.index}`}
-      onClick={() => {app.setWeekday(getDay("long")); app.setIsAfternoon(props.isAfternoon); app.setDisplayCurrent(false); 
+      onClick={() => {app.setWeekday(weekday[new Date((props.day.dt+weather[0].city.timezone )* 1000).getUTCDay()]); app.setIsAfternoon(props.isAfternoon); app.setDisplayCurrent(false); 
       document.documentElement.scrollTop = 0;}}
     >
-      <p className="weekday">{getDay("short")}</p>
+      <p className="weekday">{getDay()}</p>
       <div className="image-and-details">
         <img
                   src={`/icons/${props.day.weather[0].icon}.png`}
@@ -52,6 +62,7 @@ weekday[6] = "Saturday";
             fahrenheitOn={app.fahrenheitOn}
             className="daily-overview-wind"
           ></Wind>
+          <p id="dailyPrecipitation">{Math.ceil(dailyPrecipitation)+' mm'}</p>
         </div>
       </div>
     </div>
