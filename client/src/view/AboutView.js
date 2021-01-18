b5781ebf203afc589d376275d2cf0792fc734ac8
HenrikJava <henriklarssonmail@gmail.com>
2021-01-18 11:04:11 +0100

    fix: edited the text at about page

--- client/src/view/AboutView.js
+++ client/src/view/AboutView.js
@@ -11,23 +11,23 @@ export const AboutView = () => {
         </span>
         <div className="text-wrapper">
           <p className="text-header">I build this page</p>
-          <p className="text-sub-header">Who am I....</p>
+          <p className="text-sub-header">Who am I?</p>
           <p className="text-main">
-            My name is Henrik Larsson. I currently studying a 2-year long web
+            My name is Henrik Larsson. I am currently studying a 2-year long web
             development course. This webpage is a part of the fullstack course.
-            I choose to work by the "MERN" style. It is including Mongo DB,
+            I chose to work by the "MERN" style. It is including Mongo DB,
             Express JS, React JS and Node JS.{" "}
           </p>
           <p className="text-sub-header">Why a weather page? </p>
           <p className="text-main">
-            Ever since I was a child I always have an interest of the weather.
-            Sometimes I had felt that the weather apps have some imporovements
-            to do. Now I had the oppurtonity to build my own app.
+            Ever since I was a child I always had an interest of the weather.
+            Sometimes I have had felt that the weather apps have some improvements
+            to make. And with my new knowledge I have the opportunity to build my own web page.
           </p>
-          <p className="text-sub-header">Credits</p>
+          <p className="text-sub-header">Sources</p>
           <p className="text-main">
             The weather data is brought by http://openweathermap.org/. The
-            sloths is created by a webteam in Gothenburg(Anne-Lie-Back, ozckarr,
+            sloths are created by a webteam in Gothenburg(Anne-Lie-Back, ozckarr,
             alexbryw). The weather icons are grabbed from www.icons.com.{" "}
           </p>
         </div>
