fcead1dac4e2e7f9fcbb88727006f56167432c27
HenrikJava <henriklarssonmail@gmail.com>
2021-01-19 11:31:28 +0100

    fix: test

--- routes/api/user.js
+++ routes/api/user.js
@@ -224,7 +224,7 @@ router.put(
 //Multer used for save photo on disk.
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve(__dirname, 'build'));
+    cb(null, path.resolve(__dirname, 'build/images'));
   },
   filename: function (req, file, cb) {
 
