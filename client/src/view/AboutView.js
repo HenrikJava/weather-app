61d0c7c55bb81b9708ffbb78bba3686c03333270
HenrikJava <henriklarssonmail@gmail.com>
2021-02-09 14:58:45 +0100

    reafactor: corrected the english

--- client/src/view/AboutView.js
+++ client/src/view/AboutView.js
@@ -21,7 +21,7 @@ export const AboutView = () => {
           <p className="text-main">
             {app.swedish
               ? 'Jag heter Henrik Larsson. Jag studerar just nu en 2 år lång systemutvecklingskurs. Denna hemsida är en del av fullstackkursen. Jag valde att arbeta med "MERN" stilen. Den inkluderar Mongo DB, Express JS, React JS och Node JS.'
-              : 'My name is Henrik Larsson. I am currently studying a 2-year long web development course. This webpage is a part of the fullstack course. I chose to work by the "MERN" style. It is including Mongo DB, Express JS, React JS and Node JS.'}
+              : 'My name is Henrik Larsson. I am currently studying a 2 year long web development course. This website is part of the full stack course. I chose to work with the "MERN" style. It includes Mongo DB, Express JS, React JS and Node JS.'}
           </p>
           <p className="text-sub-header">
             {app.swedish ? "Varför en vädersida?" : "Why a weather page?"}
@@ -29,7 +29,7 @@ export const AboutView = () => {
           <p className="text-main">
             {app.swedish
               ? "Redan sen jag var barn har jag alltid varit intresserad av vädret. Ibland har jag kännt att väderapparna har en del förbättringar att göra. Och med min nya kunskap har jag nu möjligheten att bygga min egna sida."
-              : "Ever since I was a child I always had an interest of the weather. Sometimes I have had felt that the weather apps have some improvements to make. And with my new knowledge I have the opportunity to build my own web page."}
+              : "Since I was a kid, I've always been interested in the weather. Sometimes I've felt that the weather apps have some improvements to make. And with my new knowledge, I now have the opportunity to build my own page."}
           </p>
           <p className="text-sub-header">
             {app.swedish ? "Källor" : "Sources"}
@@ -37,7 +37,7 @@ export const AboutView = () => {
           <p className="text-main">
             {app.swedish
               ? "Väderdatan hämtas från http://openweathermap.org/. Sengångarna är skapade av ett webteam i Göteborg(Anne-Lie-Back, ozckarr, alexbryw). Väderikonerna är hämtade från www.icons.com."
-              : "The weather data is brought by http://openweathermap.org/. The sloths are created by a webteam in Gothenburg(Anne-Lie-Back, ozckarr, alexbryw). The weather icons are grabbed from www.icons.com."}
+              : "The weather data is retrieved from http://openweathermap.org/. The sloths are created by a web team in Gothenburg (Anne-Lie-Back, ozckarr, alexbryw). The weather icons are taken from www.icons.com."}
           </p>
         </div>
       </div>
